1ab6ecbc2a0acb0eb84f10e03aa4d8c25485914e
Taher Ben sedrine <tbns1919@gmail.com>
2025-05-20 22:50:01 -0400

    up

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -13,9 +13,10 @@ import { Footer } from "@/components/Footer";
 import { useLanguage } from "@/context/LanguageContext";
 import { Button } from "@/components/ui/button";
 import { Menu } from "lucide-react";
+;
 
 import {
-  Sheet,
+  Sheet, 
   SheetContent,
   SheetTrigger,
 } from "@/components/ui/sheet";
@@ -40,19 +41,15 @@ import {
           {/* Logo stylisé */}
           <div className="flex items-center">
             <a href="#" className="flex items-center space-x-2">
-              <svg
-                className="w-6 h-6 text-skyblue"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth={2}
-                viewBox="0 0 24 24"
-              >
-                <path d="M3 9l9-7 9 7v11a2 2 0 01-2 2H5a2 2 0 01-2-2z" />
-                <path d="M9 22V12h6v10" />
-              </svg>
-              <span className="text-xl font-bold text-skyblue tracking-tight">
-                DrainPro
-              </span>
+            
+            
+              <img
+  src= "../../public/assets/Drain-xpress.png" // Assure-toi du bon chemin
+  alt="Logo Drain-Xpress"
+  className="h-20 w-auto object-contain "
+/>
+
+
             </a>
           </div>
 
